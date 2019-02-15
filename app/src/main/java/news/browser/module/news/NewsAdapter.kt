1ed774c5861c0daa22e1f02db0efab5e3f568fd0
aaa <1289181391@qq.com>
2019-02-16 06:07:33 +0800

    optimization-beta03

--- app/src/main/java/news/browser/module/news/NewsAdapter.kt
+++ app/src/main/java/news/browser/module/news/NewsAdapter.kt
@@ -2,15 +2,17 @@ package news.browser.module.news
 
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_news.view.*
 import news.browser.R
-import news.browser.base.BaseViewHolder
 import news.browser.module.news.model.NewsBean
 import news.browser.module.web.WebActivity
 
 class NewsAdapter(private var news: ArrayList<NewsBean>) :
-        RecyclerView.Adapter<BaseViewHolder>() {
+        RecyclerView.Adapter<NewsAdapter.BaseViewHolder>() {
     fun addData(data: List<NewsBean>) {
         this.news.addAll(data)
         notifyItemRangeChanged(this.news.size - data.size, data.size)
@@ -23,17 +25,21 @@ class NewsAdapter(private var news: ArrayList<NewsBean>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = BaseViewHolder(LayoutInflater
             .from(parent.context).inflate(R.layout.item_news, parent, false))
+
     override fun getItemCount() = news.size
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         val context = holder.itemView.context
-        holder.setText(R.id.news_title, news[position].title)
-        holder.setText(R.id.news_time, news[position].updateTime)
-        holder.setImageWithUrl(R.id.news_head_img, news[position].picUrl)
+        val itemView = holder.itemView
+        itemView.news_title.text = news[position].title
+        itemView.news_time.text = news[position].updateTime
+        Glide.with(context).load(news[position].picUrl).into(itemView.news_head_img)
         holder.itemView.setOnClickListener {
             val intent = Intent(context, WebActivity::class.java)
             intent.putExtra("url", news[position].url)
             context.startActivity(intent)
         }
     }
+
+    class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
--- app/src/main/java/news/browser/module/web/WebActivity.kt
+++ app/src/main/java/news/browser/module/web/WebActivity.kt
@@ -1,9 +1,8 @@
 package news.browser.module.web
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
@@ -18,21 +17,25 @@ class WebActivity : AppCompatActivity() {
     override fun onCreate(onSavedInstanceState: Bundle?) {
         super.onCreate(onSavedInstanceState)
         setContentView(R.layout.activity_web)
-        val webSettings = webView.settings
-        webSettings.javaScriptEnabled = true
-        webSettings.setSupportZoom(true)
-        webSettings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
-        webSettings.setAppCacheEnabled(true)
-        webView.loadUrl(intent.getStringExtra("url"))
-        webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-                view.loadUrl(request.url.toString())
-                return super.shouldOverrideUrlLoading(view, request)
-            }
+        webView.settings.apply {
+            javaScriptEnabled = true
+            setSupportZoom(true)
+            layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+            setAppCacheEnabled(true)
+        }
+        webView.apply {
+            loadUrl(intent.getStringExtra("url"))
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView,
+                                                      request: WebResourceRequest): Boolean {
+                    view.loadUrl(request.url.toString())
+                    return super.shouldOverrideUrlLoading(view, request)
+                }
 
-            override fun onPageFinished(view: WebView, url: String) {
-                super.onPageFinished(view, url)
-                loadCircle.visibility = View.GONE
+                override fun onPageFinished(view: WebView, url: String) {
+                    super.onPageFinished(view, url)
+                    loadCircle.visibility = View.GONE
+                }
             }
         }
     }
